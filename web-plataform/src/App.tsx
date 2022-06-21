e53f740724264aa497ad2e7aefcbc20baa9f000a
Murilo Rodegheri <murilorodegherii@gmail.com>
2022-06-21 00:13:12 -0300

    feat: query with apollo

--- web-plataform/src/App.tsx
+++ web-plataform/src/App.tsx
@@ -1,5 +1,33 @@
-function App() {
-  return <h1 className='text-5xl'>hello world</h1>
+import { useEffect } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import { client } from './lib/apollo'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+}
+
+const App = () => {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
+  console.log(data)
+
+  return (
+    <ul>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
 }
 
 export default App
