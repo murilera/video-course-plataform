851e81f8c26874a390bcdb3d89740fb6c4a54ffe
Murilo Rodegheri <murilorodegherii@gmail.com>
2022-06-24 00:53:04 -0300

    refactor: reorganize providers

--- web-plataform/src/App.tsx
+++ web-plataform/src/App.tsx
@@ -1,5 +1,14 @@
-import { Event } from './pages/Event'
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { client } from './lib/apollo'
 
 export const App = () => {
-  return <Event />
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
 }
